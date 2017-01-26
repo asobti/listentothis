00851b8c168e9cc712e997735b3befbe4c5a0dc9
Ayush Sobti <ayush@box.com>
2017-01-26 14:16:00 -0800

    Wired up to Spotify API to add tracks to playlist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 from reddit import Reddit
 from spotify import Spotify
+import sys
 
 class Entity:
     def __init__(self, reddit_title):
@@ -14,7 +15,11 @@ class Entity:
                     self.search_term.encode('utf-8') if self.search_term else 'None',
                     self.spotify_track
                 )
+def info(msg):
+    print '[INFO ] {}'.format(msg)
 
+def error(msg):
+    print '[ERROR] {}'.format(msg)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Add subreddit top tracks to a Spotify playlist')
@@ -25,18 +30,16 @@ def parse_args():
                         help='The time period for top reddit threads')
     parser.add_argument('--limit', dest='limit', type=int, default=25,
                         help='Number of threads to retreive')
-    parser.add_argument('--refresh-token', dest='refresh_token', required=True,
-                        help='Spotify API refresh token')
     parser.add_argument('--playlist-id', dest='playlist_id', required=True,
                         help='ID of Spotify Playlist to add tracks to')
-    parser.add_argument('--append-to-playlist', dest='append_to_playlist', action='store_true',
-                        help='When set, tracks will be added to the playlist in addition to existing tracks')
     parser.add_argument('--spotify-client-id', dest='spotify_client_id', required=True,
                         help='Spotify application client ID')
     parser.add_argument('--spotify-client-secret', dest='spotify_client_secret', required=True,
                         help='Spotify application client secret')
     parser.add_argument('--refresh-token-file', dest='refresh_token_file', required=True,
                         help='Path to file with refresh token')
+    parser.add_argument('--search-threshold', dest='search_threshold', type=float, default=0.5,
+                        help='Fraction of reddit threads that must convert to Spotify tracks in order to proceed')
 
     return parser.parse_args()
 
@@ -69,30 +72,59 @@ def read_refresh_token(path):
     with open(path, 'r') as f:
         return f.read()
 
-if __name__ == "__main__":
+def main():
     options = parse_args()
 
     r = Reddit(options.subreddit)
-    top_titles = r.top(options.period, options.limit)
 
+    info('Fecthing threads from Reddit')
+    top_titles = r.top(options.period, options.limit)
     entities = [Entity(title) for title in top_titles]
+    info('Found {} threads'.format(len(entities)))
     
     for entity in entities:
         try:
             entity.search_term = search_term_from_title(entity.reddit_title)
         except:
-            print 'Failed to convert Reddit title "{}" to a search term'.format(title)
+            error('Failed to convert Reddit title "{}" to a search term'.format(title))
 
     refresh_token = read_refresh_token(options.refresh_token_file)
-    s = Spotify(options.spotify_client_id, options.spotify_client_secret, refresh_token)
 
+    try:
+        s = Spotify(options.spotify_client_id, options.spotify_client_secret, refresh_token)
+    except Exception as e:
+        error('Failed to create Spotify agent')
+        error(e)
+        return 1
+
+    info('Searching Spotify for tracks')
     for entity in entities:
         try:
             entity.spotify_track = s.search_track(entity.search_term)
         except Exception as e:
-            print e
+            error(e)
+            error('Skipping...')
 
-    for entity in entities:
-        print entity
+    tracks_found = [entity.spotify_track for entity in entities if entity.spotify_track is not None]
+    info('Found {} Spotify tracks'.format(len(tracks_found)))
+
+    if not (float(len(tracks_found)) / len(entities)) > options.search_threshold:
+        error('Search of Spotify tracks under threshold of {}'.format(options.search_threshold))
+        return 1
+
+    try:
+        info('Removing existing tracks from playlist')
+        s.clear_playlist(options.playlist_id)
+        info('Adding {} new tracks to playlist'.format(len(tracks_found)))
+        s.add_tracks_to_playlist(options.playlist_id, tracks_found)
+    except Exception as e:
+        error(e)
+        return 1
+
+    info('Run completed successfully')
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main()) 
 
     
--- spotify.py
+++ spotify.py
@@ -5,12 +5,14 @@ class SpotifyException(Exception):
 
 class Spotify:
     ApiBaseUrl = 'https://api.spotify.com'
+    ApiVersion = 'v1'
     AccountsBaseUrl = 'https://accounts.spotify.com'
 
     def __init__(self, client_id, client_secret, refresh_token):
         self.client_id = client_id.strip()
         self.client_secret = client_secret.strip()
         self.refresh_token = refresh_token.strip()
+        self.user_id = None
         self.__access_token = None
 
         # get an auth token
@@ -30,26 +32,75 @@ class Spotify:
             parsed = r.json()
             self.__access_token = parsed['access_token']
 
+    def __get_user_id(self):
+        if self.user_id is None:
+            resp = self.__get('me', {})
+            self.user_id = resp['id']
+
+        return self.user_id
+
     def __bearer_auth_header(self):
         return {
             'Authorization': 'Bearer {}'.format(self.__access_token)
         }
 
     def search_track(self, query):
-        url = '{}/v1/search'.format(Spotify.ApiBaseUrl)
-
         params = {
             'q': query,
             'type': 'track',
             'limit': 1
         }
 
+        resp = self.__get('search', params)
+
+        tracks = resp['tracks']
+
+        if tracks['total'] == 0:
+            raise SpotifyException('No search results found for query: "{}"'.format(query))
+
+        return tracks['items'][0]['uri']
+
+    def get_playlist_tracks(self, playlist_id):
+        user_id = self.__get_user_id()
+        endpoint = 'users/{}/playlists/{}/tracks'.format(user_id, playlist_id)
+
+        resp = self.__get(endpoint, {})
+        items = resp['items']
+        uris = [item['track']['uri'] for item in items]
+        return uris
+
+    def clear_playlist(self, playlist_id):
+        current_playlist_track_uris = self.get_playlist_tracks(playlist_id)
+
+        if len(current_playlist_track_uris) == 0:
+            return
+
+        tracks = [ { "uri": track_uri } for track_uri in current_playlist_track_uris ]
+        payload = { 'tracks': tracks }
+
+        user_id = self.__get_user_id()
+        endpoint = 'users/{}/playlists/{}/tracks'.format(user_id, playlist_id)
+        url = '{}/{}/{}'.format(Spotify.ApiBaseUrl, Spotify.ApiVersion, endpoint)
+
+        r = requests.delete(url, headers=self.__bearer_auth_header(), json=payload)
+        r.raise_for_status()
+
+    def add_tracks_to_playlist(self, playlist_id, uris):
+        if len(uris) == 0:
+            raise SpotifyException('No tracks to add to playlist')
+
+        payload = { 'uris': uris }
+        
+        user_id = self.__get_user_id()
+        endpoint = 'users/{}/playlists/{}/tracks'.format(user_id, playlist_id)
+        url = '{}/{}/{}'.format(Spotify.ApiBaseUrl, Spotify.ApiVersion, endpoint)
+
+        r = requests.post(url, headers=self.__bearer_auth_header(), json=payload)
+        r.raise_for_status()
+
+    def __get(self, endpoint, params):
+        url = '{}/{}/{}'.format(Spotify.ApiBaseUrl, Spotify.ApiVersion, endpoint)
+
         r = requests.get(url, headers=self.__bearer_auth_header(), params=params)
-
         if not r.raise_for_status():
-            parsed = r.json()
-            tracks = parsed['tracks']
-            if tracks['total'] == 0:
-                raise SpotifyException('No search results found for query: "{}"'.format(query))
-
-            return tracks['items'][0]['uri']
+            return r.json()
